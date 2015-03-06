696df4fe66bb306363f94e20da27d6953cfe36fe
Thomas Midgett <tmidge20@gmail.com>
2015-03-06 12:10:12 -0500

    searchable

--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -1,8 +1,17 @@
 class TwitchController < ApplicationController
   def index
-    @response = HTTParty.get("https://api.twitch.tv/kraken/channel",
+    @response = HTTParty.get("https://api.twitch.tv/kraken/search/channels?q=dota2&limit=25",
          :headers => {
            "Client Secret" => "#{ENV['TWITCH_TOKEN']}",
            "Client-ID" => "ec5agpzfzyczq1wskui7hv9zzrhageg"})
+  end
+  def show
+    @response = HTTParty.get("https://api.twitch.tv/kraken/search/channels?q=#{params[:id]}&limit=25",
+         :headers => {
+           "Client Secret" => "#{ENV['TWITCH_TOKEN']}",
+           "Client-ID" => "ec5agpzfzyczq1wskui7hv9zzrhageg"})
+   if @response["_total"] == 0 
+     redirect_to root_path
+   end
   end
 end
