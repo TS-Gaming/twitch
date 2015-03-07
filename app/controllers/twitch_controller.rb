7877e1b88909602d7ac9c5c807e643bc864de6e2
Thomas Midgett <tmidge20@gmail.com>
2015-03-07 12:11:59 -0500

    tweaking api interaction

--- app/controllers/twitch_controller.rb
+++ app/controllers/twitch_controller.rb
@@ -15,7 +15,7 @@ class TwitchController < ApplicationController
    end
    @ign_response = HTTParty.get("https://videogamesrating.p.mashape.com/get.php?game=#{params[:id]}",
         :headers => {
-          "X-Mashape-Key" => "8jxjMfitOnmshjPtuOrBNALxx19mp1RoJOojsnkNMwm5lykf2f",
+          "X-Mashape-Key" => "8jxjMfitOnmshjPtuOrBNALxx19mp1RoJOojsnkNMwm5lykf2f"
         })
   end
 end
